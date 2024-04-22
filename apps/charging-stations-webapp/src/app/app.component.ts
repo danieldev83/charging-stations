128c30f0eff0477455865408041b2614386d3e78
pable03 <zeo@kokott.eu>
2024-04-22 13:47:18 +0200

    Einfügen einer kleinen eingabe für PLZ und eines Dropdown Menüs zur wahl der Chargertypes

--- apps/charging-stations-webapp/src/app/app.component.ts
+++ apps/charging-stations-webapp/src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, FormsModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
 export class AppComponent {
   title = 'charging-stations-webapp';
+
+  options =[
+    {name : "tesla (High Power)", value : "High Power"},
+    {name : "IEC 62196-2 type 2 combo (Mennekes)", value: "IEC 62196-2 type 2 combo (Mennekes)" }
+
+    ];
+
 }
