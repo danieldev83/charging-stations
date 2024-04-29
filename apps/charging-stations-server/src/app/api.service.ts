25411341cd2869fe799fa6f0be1a29a31efb96ae
pable03 <zeo@kokott.eu>
2024-04-29 10:01:34 +0200

    API requests über momentan Statische latitude und Longitude werte auf der seite /api/charging-stations. +@nestjs/axios packet

--- apps/charging-stations-server/src/app/api.service.ts
+++ apps/charging-stations-server/src/app/api.service.ts
@@ -1,17 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { map } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import axios from 'axios';
+
+
 
 @Injectable()
 export class ChargingStationsService {
   constructor(private httpService: HttpService) {}
 
-  getExternalData() {
-    const apikey = "mTzed1DEhvMfauNNBhtraB0BvTOgL0yL";
-    const latitude= 50.935173;
-    const longitude= 6.953101;
-    const url = 'https://api.tomtom.com/search/2/poiSearch/Charge_Spot.json?key='+apikey+'&lat='+latitude+'&lon='+longitude+'&radius=10000'
+  getExternalData(lat: number, lng: number): Observable<any> {
+    const apikey = "";
+
+    const url = 'https://api.tomtom.com/search/2/poiSearch/Charge_Spot.json?key='+apikey+'&lat='+lat+'&lon='+lng+'&radius=25000'
 
     return this.httpService.get(url).pipe(map((resp)=>resp.data))
   }
+  getGeodataByPostCode(postCode: number) {
+    const apikey = ""
+    const url='https://api.openweathermap.org/geo/1.0/zip?zip='+postCode+',DE&appid='+apikey
+    async function getDatalat(postCode: number) {
+      axios.get(url)
+        .then(response=>{
+          console.log(response.data.lat)
+          const lat = response.data.lat;
+          console.log(response.data.lon)
+          const lon = response.data.lon;
+        })
+    }
+    getDatalat(postCode);
+
+    return this.httpService.get(url).pipe(map((resp)=>resp.data));
+  }
 }
+
--- apps/charging-stations-server/src/app/app.controller.ts
+++ apps/charging-stations-server/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ChargingStationsService } from './api.service';
 
@@ -13,7 +13,11 @@ export class AppController {
   }
   @Get('charging-stations')
   getExternalData() {
-    return this.chargingstationsService.getExternalData();
+    return this.chargingstationsService.getExternalData(50.935173,6.953101);
+  }
+  @Get('/charging-stations/:id')
+  getGeodataByPostcode(@Param('id') id: number)  {
+    return this.chargingstationsService.getGeodataByPostCode(id);
   }
 }
 
