b89c43d8938480866f96ea1216db500fa168a4aa
pable03 <zeo@kokott.eu>
2024-04-29 13:53:00 +0200

    Integration einer variablen Suche durch Eingabe der PLZ im link (api/chargingstations/53113) gibt die EV-Chargingstations in Bonn zurück.

--- apps/charging-stations-server/src/app/api.service.ts
+++ apps/charging-stations-server/src/app/api.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { map, Observable } from 'rxjs';
-import axios from 'axios';
-
+import { map } from 'rxjs';
 
 
 @Injectable()
 export class ChargingStationsService {
   constructor(private httpService: HttpService) {}
 
-  getExternalData(lat: number, lng: number): Observable<any> {
+
+  getExternalData(lat: number, lng: number) {
     const apikey = "";
 
     const url = 'https://api.tomtom.com/search/2/poiSearch/Charge_Spot.json?key='+apikey+'&lat='+lat+'&lon='+lng+'&radius=25000'
@@ -18,19 +17,14 @@ export class ChargingStationsService {
   }
   getGeodataByPostCode(postCode: number) {
     const apikey = ""
-    const url='https://api.openweathermap.org/geo/1.0/zip?zip='+postCode+',DE&appid='+apikey
-    async function getDatalat(postCode: number) {
-      axios.get(url)
-        .then(response=>{
-          console.log(response.data.lat)
-          const lat = response.data.lat;
-          console.log(response.data.lon)
-          const lon = response.data.lon;
-        })
-    }
-    getDatalat(postCode);
+    const url = 'https://api.openweathermap.org/geo/1.0/zip?zip=' + postCode + ',DE&appid=' + apikey
 
-    return this.httpService.get(url).pipe(map((resp)=>resp.data));
+    return this.httpService.get(url).pipe(map((response) => ({
+      lat: response.data.lat,
+      lon: response.data.lon
+    })));
+
+
   }
 }
 
--- apps/charging-stations-server/src/app/app.controller.ts
+++ apps/charging-stations-server/src/app/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ChargingStationsService } from './api.service';
+import {Observable} from 'rxjs';
 
 @Controller()
 export class AppController {
@@ -11,13 +12,22 @@ export class AppController {
   getData() {
     return this.appService.getData();
   }
+
   @Get('charging-stations')
   getExternalData() {
     return this.chargingstationsService.getExternalData(50.935173,6.953101);
   }
+
   @Get('/charging-stations/:id')
-  getGeodataByPostcode(@Param('id') id: number)  {
-    return this.chargingstationsService.getGeodataByPostCode(id);
+  async getGeodataByPostcode(@Param('id') id:number){
+    const {lat,lon} = await this.chargingstationsService.getGeodataByPostCode(id).toPromise();
+
+    const latitude:number = lat;
+    const longitude:number = lon;
+
+    return this.chargingstationsService.getExternalData(latitude,longitude)
+
   }
+
 }
 
